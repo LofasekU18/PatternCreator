4a438d464a8c00a12436429de212a380ad9be8ac
Lukáš Strnad <lstrnad13@gmail.com>
2025-02-25 14:56:55 +0100

    Learning

---
+++ README.md
@@ -0,0 +1,8 @@
+# PatternCreator
+
+PatternCreator is webapp created for simplify some task in Czech Replublic, when You want to sell or buy a car.
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function doSomething() {
+        if (document.getElementById("kojot").innerHTML == "Hello JavaScript") {
+                document.getElementById("kojot").innerHTML = "Kojot"
+        }
+        else {
+                document.getElementById("kojot").innerHTML = "Hello JavaScript"
+                document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+                document.getElementById("demo1").innerText = "Hello World";
+        }
+}
+function doSomethingElse() {
+
+}
+function doSomethingElse2() {
+
+}
